3746646111cc87cda05bcf225ffaac7464451153
yamamotokazunori <gaogaigar673@gmail.com>
2021-04-03 09:00:06 +0000

    [add]削除機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,7 @@
 class BooksController < ApplicationController
+
   def index
-    @books= Book.all
+    @books = Book.all
     @book = Book.new
   end
 
@@ -10,8 +11,11 @@ class BooksController < ApplicationController
 
   def create
     book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book)
+    if book.save
+       redirect_to book_path(book), notice: "Book was successfully created."
+    else
+      render :new
+    end
   end
 
   def edit
@@ -21,7 +25,13 @@ class BooksController < ApplicationController
   def update
     book = Book.find(params[:id])
     book.update(book_params)
-    redirect_to book_path(book)
+    redirect_to book_path(book), notice: "Book was successfully updated."
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
   end
 
   private
