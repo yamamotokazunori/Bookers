01c749d9a5aa236625db34efc52ba7727f6de82d
yamamotokazunori <gaogaigar673@gmail.com>
2021-04-04 06:53:48 +0000

    [add]createのバリデーション機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,11 +10,12 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    if book.save
-       redirect_to book_path(book), notice: "Book was successfully created."
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(book), notice: "Book was successfully created."
     else
-      render :new
+      @books = Book.all
+      render :index
     end
   end
 
